8a75ab511bba28bc4efc476273d388b1ef5a58c3
ADMINRG-L782QCK\James Creaton <jamescreatonn@gmail.com>
2017-11-04 12:07:57 +1100

    First commit of Halite Bot. Starter bot with basic upgrades or sorting planets by distance and prioritizing closer planets. Started work on NearbyEnemies function that will take in an Entity and return a vector of all enemies around it sorted by distance.

---
+++ MyBot.cpp
@@ -0,0 +1,86 @@
+#include "hlt/hlt.hpp"
+#include "hlt/navigation.hpp"
+
+std::vector<hlt::Move> moves;
+
+void Dock(const hlt::Map& map, const hlt::Ship& ship, const hlt::Planet& planet)
+{
+	hlt::possibly<hlt::Move> move;
+	std::vector<const hlt::Entity*> objectsInWay = hlt::navigation::objects_between(map, ship.location, planet.location);
+
+	if (objectsInWay.size() > 0)
+	{
+		//There are objects inbetween our path
+		for (int i = 0; i < objectsInWay.size(); i++) {
+			if (objectsInWay[i]->owner_id != ship.owner_id) {
+				move = hlt::navigation::navigate_ship_to_dock(map, ship, *objectsInWay[0], hlt::constants::MAX_SPEED);
+			}
+		}
+	}
+
+	if (ship.can_dock(planet)) {
+		moves.push_back(hlt::Move::dock(ship.entity_id, planet.entity_id));
+		return;
+	}
+	move = hlt::navigation::navigate_ship_to_dock(map, ship, planet, hlt::constants::MAX_SPEED);
+
+	if (move.second) {
+		moves.push_back(move.first);
+	}
+}
+
+int main() {
+	const hlt::Metadata metadata = hlt::initialize("JamesCreaton");
+	const hlt::PlayerId player_id = metadata.player_id;
+
+	const hlt::Map& initial_map = metadata.initial_map;
+
+	// We now have 1 full minute to analyse the initial map.
+	std::ostringstream initial_map_intelligence;
+	initial_map_intelligence
+		<< "width: " << initial_map.map_width
+		<< "; height: " << initial_map.map_height
+		<< "; players: " << initial_map.ship_map.size()
+		<< "; my ships: " << initial_map.ship_map.at(player_id).size()
+		<< "; planets: " << initial_map.planets.size();
+	hlt::Log::log(initial_map_intelligence.str());
+
+	for (;;) {
+		moves.clear();
+		const hlt::Map map = hlt::in::get_map();
+
+		for (const hlt::Ship& ship : map.ships.at(player_id)) {
+			if (ship.docking_status != hlt::ShipDockingStatus::Undocked) {
+				continue;
+			}
+
+			//DO A CHECK FOR ENTITIES AROUND THE PLANETS I OWN AND SEE IF THERE ARE ENEMIES
+			//IF THERE IS, ATTACK THEM
+
+
+
+			std::vector<const hlt::Planet*> planetsByDistance = map.getPlanetsByDistance(ship.location);
+
+			for (const hlt::Planet* planet : planetsByDistance) {
+				if (planet->owned) {
+
+					if (planet->owner_id == player_id) {
+						if (!planet->is_full()) {
+							Dock(map, ship, *planet);
+							break;
+						}
+						continue;
+					}
+				}
+
+				Dock(map, ship, *planet);
+
+				break;
+			}
+		}
+		if (!hlt::out::send_moves(moves)) {
+			hlt::Log::log("send_moves failed; exiting");
+			break;
+		}
+	}
+}
---
+++ hlt/map.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "map.hpp"
+#include "types.hpp"
+#include "ship.hpp"
+#include "planet.hpp"
+
+namespace hlt {
+	class Map {
+	public:
+		int map_width, map_height;
+
+		std::unordered_map<PlayerId, std::vector<Ship>> ships;
+		std::unordered_map<PlayerId, entity_map<unsigned int>> ship_map;
+
+		std::vector<Planet> planets;
+		entity_map<unsigned int> planet_map;
+
+		Map(int width, int height);
+
+		const Ship& get_ship(const PlayerId player_id, const EntityId ship_id) const {
+			return ships.at(player_id).at(ship_map.at(player_id).at(ship_id));
+		}
+
+		const Planet& get_planet(const EntityId planet_id) const {
+			return planets.at(planet_map.at(planet_id));
+		}
+
+
+		const std::vector<const hlt::Planet*> getPlanetsByDistance(const hlt::Location a_Position) const {
+			std::vector<double> distances = std::vector<double>(planets.size());
+			for (unsigned int i = 0; i < planets.size(); i++) {
+				distances[i] = a_Position.get_distance_to(planets[i].location);
+			}
+			std::vector<const hlt::Planet*> planetsSorted = std::vector<const hlt::Planet*>(planets.size());
+			for (unsigned int q = 0; q < planets.size(); q++) {
+				int index = -1;
+				double smallest = 9999;
+				for (unsigned int i = 0; i < distances.size(); i++) {
+					if (distances[i] < smallest) {
+						smallest = distances[i];
+						index = i;
+					}
+				}
+				planetsSorted[q] = &planets[index];
+				distances[index] = 9999;
+			}
+			return planetsSorted;
+		}
+
+		const std::vector<Entity> NearbyEnemyShips(Entity entity, double distanceToSearch)
+		{
+			std::vector<Entity> sortedEntities = std::vector<Entity>();
+
+			for (int i = 0; i < planets.size(); i++) {
+				if (planets[i].entity_id == entity.entity_id) {
+					continue;
+				}
+				sortedEntities[entity.location.get_distance_to(planets[i].location)] = planets[i];
+			}
+			 
+			for (auto& ship : ships[entity.owner_id]) {
+				if (ship.entity_id == entity.entity_id) {
+					continue;
+				}
+				sortedEntities[entity.location.get_distance_to(ship.location)] = ship;
+			}
+
+			return sortedEntities;
+		}
+
+	};
+}
